044e72221658c3fba18b60455788aa4951433ded
max <max@macks.cloud>
2025-03-16 20:01:43 +0100

    early cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Run { name: Option<String> },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Run { name } => {
+            println!("`run` called with {name:?}");
+        }
+    }
 }
