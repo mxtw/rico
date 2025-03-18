b0a5b1c5c3e86b298200d6a1c21e80a2bf73eaf4
max <max@macks.cloud>
2025-03-18 21:02:04 +0100

    set target ids

--- src/runtime/runtime.rs
+++ src/runtime/runtime.rs
@@ -13,19 +13,21 @@ fn write_file(path: &str, content: &str) {
     file.write_all(content.as_bytes()).unwrap();
 }
 
-fn set_uid_map(pid: Pid, uid: Uid) {
+fn set_uid_map(pid: Pid, uid: Uid, target_uid: Uid) {
     let path = format!("/proc/{}/uid_map", pid);
-    let uid_map = format!("0 {} 1\n", uid);
+    let uid_map = format!("{} {} 1\n", target_uid, uid);
     println!("{}", uid_map);
 
     write_file(&path, uid_map.to_string().as_str());
 }
-fn set_gid_map(pid: Pid, gid: Gid) {
+
+fn set_gid_map(pid: Pid, gid: Gid, target_gid: Gid) {
     let path = format!("/proc/{}/gid_map", pid);
-    let gid_map = format!("0 {} 1\n", gid);
+    let gid_map = format!("{} {} 1\n", target_gid, gid);
 
     write_file(&path, &gid_map);
 }
+
 fn setgroups(pid: Pid) {
     write_file(&format!("/proc/{}/setgroups", pid), &"deny\n");
 }
@@ -48,8 +50,10 @@ pub fn run_process(command: CString, args: Vec<CString>) {
             println!("waiting for child {} to exit...", child);
 
             setgroups(child);
-            set_uid_map(child, uid);
-            set_gid_map(child, gid);
+
+            // TODO: allow setting target *ids per flag
+            set_uid_map(child, uid, Into::into(0));
+            set_gid_map(child, gid, Into::into(0));
 
             match waitpid(child, None) {
                 Ok(WaitStatus::Exited(_, status)) => {
