92eaf5c18eba156630d01db893a5eb74d51dbd1a
max <max@macks.cloud>
2025-03-19 18:53:12 +0100

    pivot root into container

--- src/runtime/runtime.rs
+++ src/runtime/runtime.rs
@@ -1,15 +1,16 @@
 use std::ffi::CString;
+use std::fs::{create_dir, remove_dir};
 use std::io::Write;
+use std::path::Path;
 use std::{fs, process};
 
+use nix::mount::{mount, umount, umount2, MntFlags, MsFlags};
 use nix::sched::{unshare, CloneFlags};
 use nix::sys::wait::{waitpid, WaitStatus};
-use nix::unistd::chroot;
+use nix::unistd::{chdir, chroot, pivot_root};
 use nix::unistd::{execve, fork, getgid, getpid, getuid, ForkResult, Gid, Pid, Uid};
 
 fn write_file(path: &str, content: &str) {
-    println!("{}", path);
-
     let mut file = fs::OpenOptions::new().write(true).open(path).unwrap();
     file.write_all(content.as_bytes()).unwrap();
 }
@@ -17,7 +18,6 @@ fn write_file(path: &str, content: &str) {
 fn set_uid_map(pid: Pid, uid: Uid, target_uid: Uid) {
     let path = format!("/proc/{}/uid_map", pid);
     let uid_map = format!("{} {} 1\n", target_uid, uid);
-    println!("{}", uid_map);
 
     write_file(&path, uid_map.to_string().as_str());
 }
@@ -33,7 +33,32 @@ fn setgroups(pid: Pid) {
     write_file(&format!("/proc/{}/setgroups", pid), &"deny\n");
 }
 
-pub fn run_process(command: CString, args: Vec<CString>) {
+fn change_root(new_root: &str) -> Result<(), std::io::Error> {
+    mount(
+        Some(new_root),
+        new_root,
+        None::<&str>,
+        MsFlags::MS_BIND,
+        None::<&str>,
+    )
+    .expect("could not mount new_root");
+
+    let old_root = format!("{}/old_root", new_root);
+
+    println!("new: {:?} old: {:?}", new_root, old_root);
+
+    create_dir(&old_root)?;
+
+    pivot_root(new_root, old_root.as_str()).expect("could not pivot_root into new_root");
+    chdir("/").expect("could not chdir into new_root");
+    umount2("/old_root", MntFlags::MNT_DETACH).expect("could not unmount old_root");
+
+    remove_dir("/old_root").expect("could not unmount old_root");
+
+    Ok(())
+}
+
+pub fn run_process(command: CString, args: Vec<CString>) -> Result<(), Box<dyn std::error::Error>> {
     let flags = CloneFlags::CLONE_NEWPID
         | CloneFlags::CLONE_NEWUTS
         | CloneFlags::CLONE_NEWNS
@@ -63,6 +88,7 @@ pub fn run_process(command: CString, args: Vec<CString>) {
                 Ok(_) => eprintln!("child did not exit as expected"),
                 Err(err) => eprintln!("error waiting for child: {}", err),
             }
+            Ok(())
         }
         Ok(ForkResult::Child) => {
             println!("child pid: {}", getpid());
@@ -73,9 +99,8 @@ pub fn run_process(command: CString, args: Vec<CString>) {
             // using
             // https://dl-cdn.alpinelinux.org/alpine/v3.21/releases/x86_64/alpine-minirootfs-3.21.3-x86_64.tar.gz
             // for testing
-            chroot("./alpine").unwrap();
-
-            let _ = execve(&command, &args, &env);
+            change_root("./alpine")?;
+            execve(&command, &args, &env)?;
 
             // this should never be reached, as execve replaces the child
             eprintln!("execve failed");
